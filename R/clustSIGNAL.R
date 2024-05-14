c868166df7d3b5288991d7048459d8a5b916dba9
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2024-05-14 20:45:21 +1000

    Updated code.

--- R/clustSIGNAL.R
+++ R/clustSIGNAL.R
@@ -6,13 +6,12 @@
 #' @param spe SpatialExperiment object with logcounts and PCA included.
 #' @param samples a character vector of sample names to which the cells belong. Length of vector must be equal to the number of cells in SpatialExperiment object (i.e. the number of rows in colData(spe)).
 #' @param cells a character vector of cell IDs of each cell. Length of vector must be equal to the number of cells in SpatialExperiment object (i.e. the number of rows in colData(spe)).
-#' @param celltypes a character vector of cell type annotation of each cell. Length of vector must be equal to the number of cells in SpatialExperiment object (i.e. the number of rows in colData(spe)).
 #' @param NN an integer for the number of neighbourhood cells the function should consider. The value must be greater than or equal to 1. Default value is 30.
 #' @param kernel a character for type of distribution to be used. The two valid values are "G" or "E". G for Gaussian distribution, and E for exponential distribution. Default value is "G".
 #' @param spread a numeric value for distribution spread, represented by standard deviation for Gaussian distribution and rate for exponential distribution. Default value is 0.05 for Gaussian distribution and 20 for exponential distribution.
 #' @param sort a logical parameter for whether or not to sort the neighbourhood after region description. Default value is TRUE.
 #' @param threads a numeric value for the number of CPU cores to be used for the analysis. Default value set to 4 cores.
-#' @param outputs a character for the type of output to return to the user. "pm" for plots and metrics (default), "n" for plots and metrics plus neighbourhood matrix, "s" for plots and metrics plus final spatialExperiment object, or "a" for all outputs.
+#' @param outputs a character for the type of output to return to the user. "c" for data frame of cell IDs and their respective cluster numbers (default), "n" for dataframe of clusters plus neighbourhood matrix, "s" for  dataframe of clusters plus final spatialExperiment object, or "a" for all outputs.
 #'
 #' @return a list of outputs
 #'
@@ -39,34 +38,25 @@
 clustSIGNAL <- function (spe,
                          samples,
                          cells,
-                         celltypes,
                          NN = 30,
                          kernel = "G",
                          spread = 0.05,
                          sort = TRUE,
                          threads = 4,
-                         outputs = "pm") {
+                         outputs = "c") {
 
-    require(aricode)
     require(BiocNeighbors)
     require(bluster)
-    require(circlize)
-    require(cluster)
-    require(ComplexHeatmap)
-    require(distances)
     require(DescTools)
+    require(dplyr)
     require(foreach)
-    require(ggplot2)
-    require(ggspavis)
     require(Hmisc)
     require(parallel)
     require(PCAtools)
-    require(plotly)
     require(scater)
     require(scran)
     require(scuttle)
     require(SpatialExperiment)
-    require(RColorBrewer)
 
     if (NN < 1){
         stop("ERROR: Number of nearest neighbours cannot be less than 1.")
@@ -83,58 +73,32 @@ clustSIGNAL <- function (spe,
 
     # Domainness measure and spread
     spe <- entropyMeasure(spe, cells, outReg$regXclust, threads)
-    # entropy histogram and spatial plots
-    domPlotList <- .plots(spe, samples, celltypes, plotType = "domain")
 
     # Smoothing
-    spe <- adaptiveSmoothing(spe, outReg$nnCells, NN, kernel, spread, cells, threads)
+    spe <- adaptiveSmoothing(spe, outReg$nnCells, NN, kernel, spread)
 
     # Non-spatial reclustering
     # reclust should always be TRUE here
     spe <- nsClustering(spe, reclust = TRUE)
-    # spatial plots of annotated and cluster labels
-    spatialPlot <- .plots(spe, samples, celltypes, plotType = "spatial")
 
-    # Cluster metrics
-    met <- .metrics(spe, samples, celltypes, cells)
-    # heat map comparison between annotated and cluster labels
-    cellHeat <- .plots(met$spe_out, samples, celltypes, plotType = "heatmap")
+    cluster_df <- as.data.frame(cells) %>%
+        cbind(as.data.frame(spe$reCluster))
 
     print(paste("Adaptive clustering run completed. Outputs are ready.", Sys.time()))
 
-    if (outputs == "ps"){
-        return (list("domainSpread" = domPlotList$spread,
-                     "domainDistribution" = domPlotList$distribution,
-                     "spatialAnnotated" = spatialPlot$cellPlot,
-                     "spatialCluster" = spatialPlot$clusterPlot,
-                     "compareHeatmap" = cellHeat,
-                     "metrics" = met$metrics_out))
+    if (outputs == "c"){
+        return (cluster_df)
     } else if (outputs == "n"){
-        return (list("domainSpread" = domPlotList$spread,
-                     "domainDistribution" = domPlotList$distribution,
-                     "spatialAnnotated" = spatialPlot$cellPlot,
-                     "spatialCluster" = spatialPlot$clusterPlot,
-                     "compareHeatmap" = cellHeat,
-                     "metrics" = met$metrics_out,
+        return (list("clusters" = cluster_df,
                      "neighbours" = outReg$nnCells))
     } else if (outputs == "s") {
-        return (list("domainSpread" = domPlotList$spread,
-                     "domainDistribution" = domPlotList$distribution,
-                     "spatialAnnotated" = spatialPlot$cellPlot,
-                     "spatialCluster" = spatialPlot$clusterPlot,
-                     "compareHeatmap" = cellHeat,
-                     "metrics" = met$metrics_out,
-                     "spe_final" = met$spe_out))
+        return (list("clusters" = cluster_df,
+                     "spe_final" = spe))
     } else if (outputs == "a") {
-        return (list("domainSpread" = domPlotList$spread,
-                     "domainDistribution" = domPlotList$distribution,
-                     "spatialAnnotated" = spatialPlot$cellPlot,
-                     "spatialCluster" = spatialPlot$clusterPlot,
-                     "compareHeatmap" = cellHeat,
-                     "metrics" = met$metrics_out,
+        return (list("clusters" = cluster_df,
                      "neighbours" = outReg$nnCells,
-                     "spe_final" = met$spe_out))
+                     "spe_final" = spe))
     } else {
-        print("ERROR: invalid character used. Use 'ps', 'n', 's', or 'a' to indicate the type of output you would like." )
+        print("ERROR: invalid character used. Use 'c', 'n', 's', or 'a' to indicate the type of output you would like." )
     }
 }
