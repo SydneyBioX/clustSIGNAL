bb08c2c4476025ef66a623c2cc753953d81327be
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2024-05-29 14:11:02 +1000

    Modified clustSIGNAL function - default number of threads are now 1, default output will be a list, '...' functionality added to allow users to pass additional parameters to clustering component, and overall function description was updated.

--- R/clustSIGNAL.R
+++ R/clustSIGNAL.R
@@ -12,40 +12,34 @@
 #' @param sort a logical parameter for whether or not to sort the neighbourhood after region description. Default value is TRUE.
 #' @param threads a numeric value for the number of CPU cores to be used for the analysis. Default value set to 4 cores.
 #' @param outputs a character for the type of output to return to the user. "c" for data frame of cell IDs and their respective cluster numbers (default), "n" for dataframe of clusters plus neighbourhood matrix, "s" for  dataframe of clusters plus final spatialExperiment object, or "a" for all outputs.
+#' @param ... additional parameters for TwoStepParam clustering methods. Include parameters for k for number of nearest neighbours and cluster.fun for selecting community detection method. Default values k = 5, cluster.fun = "louvain".
 #'
 #' @return a list of outputs
 #'
-#' 1. domainSpread: a histogram of entropy values and number of cell neighbourhoods.
+#' 1. clusters: a data frame of cell names and their cluster classification.
 #'
-#' 2. domainDistribution: a spatial plot showing entropy of all cell neighbourhoods.
+#' 2. neighbours: a matrix of cell names and the names of their NN nearest neighbour cells.
 #'
-#' 3. spatialAnnotated: a spatial plot of annotated cell types. If no celltype annotations were provides, the output will be NULL.
-#'
-#' 4. spatialCluster: a spatial plot of clusters generated using clustSIGNAL.
-#'
-#' 5. compareHeatmap: a heat map showing cell number distributions of annotated cell type and output clusters. If no celltype annotations were provides, the output will be NULL.
-#'
-#' 6. metrics: a table with ARI, NMI, mean silhouette width, minimum entropy, maximum entropy, and mean entropy of each sample in the dataset. If no celltype annotations were provides, the ARI and NMI values will be NULL.
-#'
-#' 7. neighbours: a matrix of cell names and the names of their NN nearest neighbour cells.
-#'
-#' 8. spe_final: a SpatialExperiment object with initial 'putative cell type' groups, entropy values, smoothed gene expression, post-smoothing clusters, and silhouette widths included.
+#' 3. spe_final: a SpatialExperiment object with initial 'putative cell type' groups, entropy values, smoothed gene expression, post-smoothing clusters, and silhouette widths included.
 #'
 #' @examples
+#' spe = load(mouseEmbryo2)
+#' clustSIGNAL <- function (spe, spe$embryo, spe$uniqueIDs)
 #'
-#' # to do
 #'
 #' @export
 
 clustSIGNAL <- function (spe,
                          samples,
                          cells,
+                         dimRed = "PCA",
                          NN = 30,
                          kernel = "G",
                          spread = 0.05,
                          sort = TRUE,
-                         threads = 4,
-                         outputs = "c") {
+                         threads = 1,
+                         outputs = "c",
+                         ...) {
 
     # require(BiocNeighbors)
     # require(bluster)
@@ -64,11 +58,11 @@ clustSIGNAL <- function (spe,
         stop("ERROR: Number of nearest neighbours cannot be less than 1.")
     }
 
-    print(paste("Adaptive clustering run started.", Sys.time()))
+    print(paste("clustSIGNAL run started.", Sys.time()))
 
     # Non-spatial clustering
     # reclust should always be FALSE here
-    spe <- nsClustering(spe, reclust = FALSE)
+    spe <- nsClustering(spe, dimRed, reclust = FALSE, ...)
 
     # Region description and sorting, if sort = "yes"
     outReg <- neighbourDetect(spe, samples, NN, cells, sort)
@@ -81,15 +75,14 @@ clustSIGNAL <- function (spe,
 
     # Non-spatial reclustering
     # reclust should always be TRUE here
-    spe <- nsClustering(spe, reclust = TRUE)
+    spe <- nsClustering(spe, reclust = TRUE, ...)
 
-    cluster_df <- as.data.frame(cells) %>%
-        cbind(as.data.frame(spe$reCluster))
+    cluster_df <- data.frame("Cells" = cells, "Clusters" = spe$reCluster)
 
-    print(paste("Adaptive clustering run completed. Outputs are ready.", Sys.time()))
+    print(paste("clustSIGNAL run completed.", Sys.time()))
 
     if (outputs == "c"){
-        return (cluster_df)
+        return (list("clusters" = cluster_df))
     } else if (outputs == "n"){
         return (list("clusters" = cluster_df,
                      "neighbours" = outReg$nnCells))
