af2359e17b1a7269d0752d21911a266ba1b3726e
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2024-05-29 14:06:33 +1000

    Added options for user-defined dimensionality reduction parameter. If no dimensionality reduction was performed on the input dataset, PCA will be performed by default.

--- R/clustering.R
+++ R/clustering.R
@@ -15,33 +15,38 @@
 #' @export
 
 #### Non-spatial clustering
-nsClustering <- function(spe, reclust) {
+nsClustering <- function(spe, dimRed, reclust, ...) {
     # number of centers = 1/5th of total cells in sample
     clustVal <- min(as.integer(ncol(spe) / 5), 50000)
     if (reclust == FALSE) {
-        # Clustering
-        mat <- reducedDim(spe, "PCA")
-        set.seed(12997)
-        nsClust <- clusterRows(mat, TwoStepParam(first = KmeansParam(centers = clustVal, iter.max = 30),
-                                                 second = NNGraphParam(k = 5, cluster.fun = "louvain")))
-        spe$nsCluster <- factor(nsClust)
-        print(paste("Initial nonspatial clustering performed on log counts. Clusters =", length(unique(nsClust)), Sys.time()))
-        # Subclustering
-        clusters <- length(unique(spe$nsCluster))
-        subclusters_list = list()
-        set.seed(12997)
-        for (c in 1:clusters){
-            speX <- spe[, spe$nsCluster == c]
-            speX <- runPCA(speX)
-            matX <- reducedDim(speX, "PCA")
-            # number of centers = half of total cells in cluster
-            subclustVal <- as.integer(ncol(speX) / 2)
-            nsClustX <- clusterRows(matX, TwoStepParam(first = KmeansParam(centers = subclustVal, iter.max = 30),
-                                                       second = NNGraphParam(k = 5, cluster.fun = "louvain")))
-            subclusters_list[[c]] <- setNames(paste0(c, ".", nsClustX), colnames(speX))
+        if (dimRed == "None") {
+            spe <- runPCA(spe)
         }
-        spe$nsSubcluster <- unlist(subclusters_list)[colnames(spe)]
-        print(paste("Nonspatial subclustering performed. Subclusters =", length(unique(spe$nsSubcluster)), Sys.time()))
+        else {
+            # Clustering
+            mat <- reducedDim(spe, dimRed)
+            set.seed(12997)
+            nsClust <- clusterRows(mat, TwoStepParam(first = KmeansParam(centers = clustVal, iter.max = 30),
+                                                     second = NNGraphParam(k = 5, cluster.fun = "louvain")))
+            spe$nsCluster <- factor(nsClust)
+            print(paste("Initial nonspatial clustering performed. Clusters =", length(unique(nsClust)), Sys.time()))
+            # Subclustering
+            clusters <- length(unique(spe$nsCluster))
+            subclusters_list = list()
+            set.seed(12997)
+            for (c in 1:clusters){
+                speX <- spe[, spe$nsCluster == c]
+                speX <- runPCA(speX)
+                matX <- reducedDim(speX, "PCA")
+                # number of centers = half of total cells in cluster
+                subclustVal <- as.integer(ncol(speX) / 2)
+                nsClustX <- clusterRows(matX, TwoStepParam(first = KmeansParam(centers = subclustVal, iter.max = 30),
+                                                           second = NNGraphParam(k = 5, cluster.fun = "louvain")))
+                subclusters_list[[c]] <- setNames(paste0(c, ".", nsClustX), colnames(speX))
+            }
+            spe$nsSubcluster <- unlist(subclusters_list)[colnames(spe)]
+            print(paste("Nonspatial subclustering performed. Subclusters =", length(unique(spe$nsSubcluster)), Sys.time()))
+        }
     } else if (reclust == TRUE) {
         # Reclustering
         set.seed(12997)
