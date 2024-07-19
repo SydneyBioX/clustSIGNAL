aed1f9b602438209c9dac7899a487df80b593d55
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2024-07-19 14:47:49 +1000

    Minor changes to parameters. Added data and parameter checks before clustSIGNAL run. Modified cell and sample input types from character vectors containing all values to column names in colData of spe object.

--- R/clustSIGNAL.R
+++ R/clustSIGNAL.R
@@ -4,8 +4,8 @@
 #' A clustering method for cell type classification of spatial transcriptomics data. The tool generates and uses an adaptively smoothed, spatially informed gene expression data for clustering.
 #'
 #' @param spe a SpatialExperiment object.
-#' @param samples a character vector of sample names to which the cells belong. Length of vector must be equal to the number of cells in SpatialExperiment object (i.e. the number of rows in colData(spe)).
-#' @param cells a character vector of cell IDs of each cell. Length of vector must be equal to the number of cells in SpatialExperiment object (i.e. the number of rows in colData(spe)).
+#' @param samples a character indicating name of colData(spe) column containing sample names.
+#' @param cells a character indicating name of colData(spe) column containing cell IDs.
 #' @param dimRed a character indicating the name of the reduced dimensions to use from the SpatialExperiment object (i.e., from reducedDimNames(spe)). Default value is 'PCA'.
 #' @param NN an integer for the number of neighbourhood cells the function should consider. The value must be greater than or equal to 1. Default value is 30.
 #' @param kernel a character for type of distribution to be used. The two valid values are "G" or "E". G for Gaussian distribution, and E for exponential distribution. Default value is "G".
@@ -33,7 +33,7 @@
 clustSIGNAL <- function (spe,
                          samples,
                          cells,
-                         dimRed = "PCA",
+                         dimRed = "None",
                          NN = 30,
                          kernel = "G",
                          spread = 0.05,
@@ -42,20 +42,30 @@ clustSIGNAL <- function (spe,
                          outputs = "c",
                          ...) {
 
-    # require(BiocNeighbors)
-    # require(bluster)
-    # require(DescTools)
-    # require(dplyr)
-    # require(foreach)
-    # require(Hmisc)
-    # require(parallel)
-    # require(PCAtools)
-    # require(scater)
-    # require(scran)
-    # require(SpatialExperiment)
+    # data and parameter checks
+    if (is.null(spatialCoords(spe)) == TRUE){
+        stop("The spe object should contain spatial coordinates as
+             spatialCoords matrix in the object.")
+    } else if (is.null(spe[[samples]]) == TRUE) {
+        stop("The samples variable does not match any colData column name in the spe object.")
+    } else if (is.null(spe[[cells]]) == TRUE) {
+        stop("The cells variable does not match any colData column name in the spe object.")
+    } else if (NN < 1){
+        stop("Number of nearest neighbours cannot be less than 1.")
+    } else if (!(kernel %in% c("G", "E"))) {
+        stop("Invalid character for kernel - use 'G' or 'E' to indicate the
+             weight distribution you would like to use.")
+    } else if (!(outputs %in% c('c', 'n', 's', 'a'))) {
+        stop("Invalid character for output type - use 'c', 'n', 's', or 'a' to
+              indicate the type of output you would like.")
+    }
 
-    if (NN < 1){
-        stop("ERROR: Number of nearest neighbours cannot be less than 1.")
+    if (dimRed == "None") {
+        print("Calculating PCA to use as reduced dimension input.")
+        spe <- scater::runPCA(spe)
+        dimRed = "PCA"
+    } else if (!(dimRed %in% reducedDimNames(spe))){
+        stop("The specified reduced dimension data does not exist.")
     }
 
     print(paste("clustSIGNAL run started.", Sys.time()))
@@ -81,12 +91,12 @@ clustSIGNAL <- function (spe,
     # reclust should always be TRUE here
     spe <- nsClustering(spe, reclust = TRUE, ...)
 
-    cluster_df <- data.frame("Cells" = cells, "Clusters" = spe$reCluster)
+    cluster_df <- data.frame("Cells" = spe[[cells]], "Clusters" = spe$reCluster)
 
     print(paste("clustSIGNAL run completed.", Sys.time()))
 
     if (outputs == "c"){
-        return (cluster_df)
+        return (list("clusters" = cluster_df))
     } else if (outputs == "n"){
         return (list("clusters" = cluster_df,
                      "neighbours" = outReg$nnCells))
@@ -97,7 +107,5 @@ clustSIGNAL <- function (spe,
         return (list("clusters" = cluster_df,
                      "neighbours" = outReg$nnCells,
                      "spe_final" = spe))
-    } else {
-        print("ERROR: invalid character used. Use 'c', 'n', 's', or 'a' to indicate the type of output you would like." )
     }
 }
