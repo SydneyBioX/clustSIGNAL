2e5ee6965bfb1b000df8a07ae3ec41d7be841635
PratibhaPanwar <pratibhapanwar.4@gmail.com>
2025-01-08 10:00:58 +1100

    Updated example data name and removed a test for cell label column.

--- tests/testthat/test_conditions.R
+++ tests/testthat/test_conditions.R
@@ -1,37 +1,23 @@
 test_that(
     "Expecting error when wrong column name given for samples", {
-        data(example)
+        data(ClustSignal_example)
         samples <- "wrongID"
-        cells <- "uniqueID"
 
         testthat::expect_error(
-            suppressWarnings(clustSIGNAL(spe, samples, cells))
-        )
-    }
-)
-
-test_that(
-    "Expecting error when wrong column name given for cells", {
-        data(example)
-        samples <- "sample_id"
-        cells <- "wrongID"
-
-        testthat::expect_error(
-            suppressWarnings(clustSIGNAL(spe, samples, cells))
+            suppressWarnings(clustSIGNAL(spe, samples))
         )
     }
 )
 
 test_that(
     "Expecting error when spatial coordinates missing", {
-        data(example)
+        data(ClustSignal_example)
         samples <- "sample_id"
-        cells <- "uniqueID"
         colData(spe) <- cbind(colData(spe), spatialCoords(spe))
         spatialCoords(spe) <- NULL
 
         testthat::expect_error(
-            suppressWarnings(clustSIGNAL(spe, samples, cells))
+            suppressWarnings(clustSIGNAL(spe, samples))
         )
     }
 )
