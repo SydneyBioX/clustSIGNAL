b2cceeb2c0d377250b8bc3336f99a93f3981894f
ppan0697 <pratibha.panwar@sydney.edu.au>
2026-06-02 13:27:58 +1000

    Replaced dimRed with dimRed_init specific to initial clustering. Reduced the minimum number of centers used in bluster k-means from 5000 to 3000.

--- R/initClustering.R
+++ R/initClustering.R
@@ -6,9 +6,9 @@
 #' subclusters'.
 #' @param spe a SpatialExperiment object containing spatial coordinates in
 #' 'spatialCoords' matrix and normalised gene expression in 'logcounts' assay.
-#' @param dimRed a character indicating the name of the reduced dimensions to
-#' use from the SpatialExperiment object (i.e., from reducedDimNames(spe)).
-#' Default value is 'None'.
+#' @param dimRed_init a character indicating the name of the reduced dimensions
+#' in the SpatialExperiment object (i.e., from reducedDimNames(spe)) to use for
+#' initial clustering step. Default value is 'None'.
 #' @param batch a logical parameter for whether to perform batch correction.
 #' Default value is FALSE.
 #' @param batch_by a character indicating name of colData(spe) column containing
@@ -17,7 +17,7 @@
 #' analysis. Default value set to 1.
 #' @param clustParams a list of parameters for TwoStepParam clustering methods:
 #' clust_c is the number of centers to use for clustering with KmeansParam. By
-#' default set to 0, in which case the method uses either 5000 centers or 1/5th
+#' default set to 0, in which case the method uses either 3000 centers or 1/5th
 #' of the total cells in the data as the number of centers, whichever is lower.
 #' subclust_c is the number of centers to use for sub-clustering the initial
 #' clusters with KmeansParam. The default value is 0, in which case the method
@@ -36,67 +36,83 @@
 #' @importFrom harmony RunHarmony
 #' @importFrom SummarizedExperiment colData
 #' @importFrom SingleCellExperiment reducedDim
-#' @importFrom methods show
 #' @importFrom stats setNames
+#' @importFrom BiocParallel bplapply
+#' @importFrom BiocSingular IrlbaParam
 #' @examples
 #' data(ClustSignal_example)
 #'
-#' spe <- clustSIGNAL::p1_clustering(spe, dimRed = "PCA")
+#' spe <- clustSIGNAL::p1_clustering(spe, dimRed_init = "PCA")
 #' spe$nsCluster |> head()
 #' spe$initCluster |> head()
 #' @export
 
-p1_clustering <- function(spe, dimRed = "None", batch = FALSE,
+p1_clustering <- function(spe, dimRed_init = "None", batch = FALSE,
                           batch_by = "None", threads = 1,
                           clustParams = list(clust_c = 0, subclust_c = 0,
                                              iter.max = 30, k = 10,
                                              cluster.fun = "louvain")) {
-    if (clustParams[[1]] == 0)
-        clustVal <- min(as.integer(ncol(spe) / 5), 5000) else
-            clustVal <- clustParams[[1]]
-    # Initial clustering
+
+    # extracting parameters
+    clust_c <- clustParams$clust_c
+    subclust_c <- clustParams$subclust_c
+    iter.max <- clustParams$iter.max
+    k <- clustParams$k
+    cluster.fun <- clustParams$cluster.fun
+
+    # checking availability of low dimension data
+    if (dimRed_init == "None") {
+        message(sprintf("%s Calculating PCA.", format(Sys.time(), '%H:%M:%S')))
+        spe <- runPCA(spe)
+        dimRed_init <- "PCA"
+    } else if (!(dimRed_init %in% reducedDimNames(spe))){
+        stop("Specified low dimension data not found.")}
+
+    # calculating number of cluster centres
+    clustVal <-
+        if (clust_c == 0) min(as.integer(ncol(spe) / 5), 3000) else clust_c
+
+    # checking if batch correction needed
     if (batch == TRUE) {
-        emb <- harmony::RunHarmony(data_mat = reducedDim(spe, dimRed),
-                                   meta_data = colData(spe),
-                                   vars_use = batch_by, max.iter = 20,
-                                   verbose = FALSE)
-        mat <- emb
+        mat <- RunHarmony(data_mat = reducedDim(spe, dimRed_init),
+                          meta_data = colData(spe), vars_use = batch_by,
+                          max.iter = 20, verbose = FALSE)
     } else {
-        mat <- reducedDim(spe, dimRed)
+        mat <- reducedDim(spe, dimRed_init)
     }
-    initClust <- bluster::clusterRows(mat, bluster::TwoStepParam(
-        first = bluster::KmeansParam(centers = clustVal,
-                                     iter.max = clustParams[[3]]),
-        second = bluster::NNGraphParam(k = clustParams[[4]],
-                                       cluster.fun = clustParams[[5]],
-                                       num.threads = threads)))
+
+    # initial clustering
+    initClust <- clusterRows(
+        mat,
+        TwoStepParam(
+            first = KmeansParam(centers = clustVal, iter.max = iter.max),
+            second = NNGraphParam(k = k, cluster.fun = cluster.fun,
+                                  num.threads = threads)))
     spe$initCluster <- factor(initClust)
-    show(paste("Initial nonspatial clustering performed. Clusters =",
-               length(unique(initClust)), "Time",
-               format(Sys.time(),'%H:%M:%S')))
-    # Initial subclustering
-    clusters <- length(unique(spe$initCluster))
-    subclusters_list <- list()
-    for (cl in seq_len(clusters)){
+    message(sprintf("%s Initial clustering performed. Clusters = %d",
+                    format(Sys.time(), '%H:%M:%S'), length(unique(initClust))))
+
+    # initial sub-clustering with parallel runs
+    BPPARAM <- .generateBPParam(cores = threads)
+    clusters <- unique(spe$initCluster)
+    subclusters_list <- bplapply(clusters, function(cl) {
         speX <- spe[, spe$initCluster == cl]
-        speX <- scater::runPCA(speX)
-        matX <- reducedDim(speX, "PCA")
-        if (clustParams[[2]] == 0)
-            subclustVal <- max(as.integer(ncol(speX) / 2), 1) else
-                subclustVal <- clustParams[[2]]
-        initClustX <- bluster::clusterRows(
-            matX,
-            bluster::TwoStepParam(
-                first = bluster::KmeansParam(centers = subclustVal,
-                                             iter.max = clustParams[[3]]),
-                second = bluster::NNGraphParam(k = clustParams[[4]],
-                                               cluster.fun = clustParams[[5]],
-                                               num.threads = threads)))
-        subclusters_list[[cl]] <- setNames(paste0(cl, ".", initClustX),
-                                          colnames(speX))}
+        # Using a fast approximate PCA (Irlba) instead of exact PCA
+        speX <- runPCA(speX, BSPARAM = IrlbaParam())
+        # calculating number of sub-cluster centres
+        subclustVal <-
+            if (subclust_c == 0) max(as.integer(ncol(speX) / 2), 1) else subclust_c
+        initClustX <- clusterRows(
+            reducedDim(speX, "PCA"),
+            TwoStepParam(
+                first = KmeansParam(centers = subclustVal, iter.max = iter.max),
+                second = NNGraphParam(k = k, cluster.fun = cluster.fun)))
+        return(setNames(paste0(cl, ".", initClustX), colnames(speX)))
+    }, BPPARAM = BPPARAM)
     spe$initSubcluster <- factor(unlist(subclusters_list)[colnames(spe)])
-    show(paste("Nonspatial subclustering performed. Subclusters =",
-               length(unique(spe$initSubcluster)), "Time",
-               format(Sys.time(),'%H:%M:%S')))
+    message(
+        sprintf(
+            "%s Initial sub-clustering performed. Subclusters = %d ",
+            format(Sys.time(), '%H:%M:%S'), length(unique(spe$initSubcluster))))
     return (spe)
 }
