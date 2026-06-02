6e34f946ea2cd5d68ce5246ba29b62357ec30dd2
ppan0697 <pratibha.panwar@sydney.edu.au>
2026-06-02 15:14:48 +1000

    Updated code to run on the neighbourhood composition matrix.

--- R/entropyMeasure.R
+++ R/entropyMeasure.R
@@ -6,42 +6,44 @@
 #' heterogeneous neighbourhoods have high entropy.
 #' @param spe SpatialExperiment object with initial cluster and subcluster
 #' labels.
-#' @param regXclust a list of vectors of each cell's neighbourhood composition
-#' indicated by the proportion of initial subclusters it contains.
-#' @param threads a numeric value for the number of CPU cores to be used for the
-#' analysis. Default value set to 1.
+#' @param regXclust a numeric matrix of cells by subclusters, where the values
+#' are the proportion of initial subclusters in each cell's neighbourhood.
 #' @return SpatialExperiment object with entropy values associated with each
 #' cell.
-#' @importFrom BiocParallel bplapply
-#' @importFrom methods show
 #' @examples
 #' data(ClustSignal_example)
 #'
-#' # requires list containing cluster proportions of each region (regXclust),
-#' # generated using the neighbourDetect() function
+#' # requires matrix containing cluster proportions of each neighbourhood
+#' # (regXclust), generated using the neighbourDetect() function
 #' spe <- clustSIGNAL::entropyMeasure(spe, regXclust)
 #' spe$entropy |> head()
 #' @export
 
-entropyMeasure <- function(spe, regXclust, threads = 1) {
-    cell_vect <- as.vector(colnames(spe))
-    BPPARAM <- .generateBPParam(cores = threads)
-    regEntropy <- BiocParallel::bplapply(cell_vect, function(c){
-        arr <- as.vector(unlist(regXclust[c]))
-        y <- round(matrix(-sum(arr * log2(arr)), nrow = 1, ncol = 1), 5)
-        rownames(y) <- c
-        y}, BPPARAM = BPPARAM)
-    # QC check
-    check.cells <- identical(sapply(regEntropy, rownames), colnames(spe))
-    check.NA.values <- sum(is.na(unlist(regEntropy)))
-    if (check.cells == FALSE) {
-        stop("Cell order in entropy data does not match cell order in Spatial
-             Experiment object")
-    } else if (check.NA.values != 0) {
+entropyMeasure <- function(spe, regXclust) {
+    # calculating max entropy
+    max_ent <- log2(ncol(regXclust))
+    nz_prop <- regXclust > 0
+    p_log <- matrix(0, nrow = nrow(regXclust), ncol = ncol(regXclust))
+    p_log[nz_prop] <- regXclust[nz_prop] * log2(regXclust[nz_prop])
+    regEntropy <- setNames(-rowSums(p_log), rownames(regXclust))
+    # # normalising entropy by total subclusters
+    # if (max_ent > 0) { # i.e., only 1 cluster
+    #     norm_entropy <- regEntropy / max_ent
+    # } else {
+    #     norm_entropy <- regEntropy
+    # }
+    # norm_entropy <- round(norm_entropy, 5)
+
+    # QC checks
+    if (!identical(names(regEntropy), colnames(spe))) {
+        stop("Cells have likely been allocated wrong entropy values.")
+    }
+    if (anyNA(regEntropy)) {
         stop("Entropy values are missing for some cells.")
-    } else {
-        spe$entropy <- as.vector(unlist(regEntropy))
-        show(paste("Region heterogeneity calculated. Time",
-                   format(Sys.time(),'%H:%M:%S')))}
+    }
+
+    spe$entropy <- regEntropy
+    message(sprintf("%s Neighbourhood heterogeneity calculated.",
+                    format(Sys.time(),'%H:%M:%S')))
     return(spe)
 }
